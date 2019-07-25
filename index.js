79d6f0702d927cd042d2f8b85cfd9738805d7cea
Cesar <cesar2012oliveira@gmail.com>
2019-07-25 17:45:36 -0300

    custom animation type working

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var elements = document.querySelectorAll('*[data-motion]');
 
 function initAnimation(element) {
+  // console.log(element.dataset.motion);
+  var animationType = element.dataset.motion;
   var io = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       const { intersectionRatio } = entry;
       if (intersectionRatio > 0) {
-        element.classList.remove('hidden')
-        element.classList.add('animated');
+        element.setAttribute('data-motion', animationType);
+        io.unobserve(entry.target);
       } else {
-        element.classList.remove('animated');
-        element.classList.add('hidden');
+        // element.classList.remove('animated');
+        element.removeAttribute('data-motion')
+        // element.classList.add('hidden');
       };
     })
   });
