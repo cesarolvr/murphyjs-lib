66959ab9f3d2ebe5f529904d5d0a73b01d6aaac5
Cesar <cesar2012oliveira@gmail.com>
2019-08-05 16:50:38 -0300

    bug resolved in support verification

--- src/index.js
+++ src/index.js
@@ -1,45 +1,48 @@
-var LEFT_TO_RIGHT = 'left-to-right'
-var RIGHT_TO_LEFT = 'right-to-left'
-var TOP_TO_BOTTOM = 'top-to-bottom'
-var BOTTOM_TO_TOP = 'bottom-to-top'
+var LEFT_TO_RIGHT = "left-to-right";
+var RIGHT_TO_LEFT = "right-to-left";
+var TOP_TO_BOTTOM = "top-to-bottom";
+var BOTTOM_TO_TOP = "bottom-to-top";
 
-var murphySelector = '[data-murphy]'
+var murphySelector = "[data-murphy]";
 
 function playMurphy() {
-  if (!observerIsSupported()) cancelMurphy()
-  var elements = document.querySelectorAll(murphySelector)
-  elements.forEach(element => {
-    startAnimation(element)
-  })
+  if (observerIsSupported()) {
+    var elements = document.querySelectorAll(murphySelector);
+    return elements.forEach(element => {
+      startAnimation(element);
+    });
+  } else {
+    return cancelMurphy();
+  }
 }
 
 function cancelMurphy() {
-  var elements = document.querySelectorAll(murphySelector)
+  var elements = document.querySelectorAll(murphySelector);
   elements.forEach(element => {
-    element.style.opacity = 1
-  })
+    element.style.opacity = 1;
+  });
 }
 
 function resetMurphy() {
-  var elements = document.querySelectorAll(murphySelector)
-  resetElements(elements)
+  var elements = document.querySelectorAll(murphySelector);
+  resetElements(elements);
 }
 
 function startAnimation(element) {
-  var animationType = element.dataset.murphy || BOTTOM_TO_TOP
-  var appearanceDistance = element.dataset.murphyAppearanceDistance || 50
-  var elementDistance = element.dataset.murphyElementDistance || 30
-  var ease = element.dataset.murphyEase || 'ease'
-  var delay = parseInt(element.dataset.murphyAnimationDelay) || 300
-  var elementThreshold = parseInt(element.dataset.murphyElementThreshold) || 1
+  var animationType = element.dataset.murphy || BOTTOM_TO_TOP;
+  var appearanceDistance = element.dataset.murphyAppearanceDistance || 50;
+  var elementDistance = element.dataset.murphyElementDistance || 30;
+  var ease = element.dataset.murphyEase || "ease";
+  var delay = parseInt(element.dataset.murphyAnimationDelay) || 300;
+  var elementThreshold = parseInt(element.dataset.murphyElementThreshold) || 1;
 
   var animationDuration =
-    parseInt(element.dataset.murphyAnimationDuration) || 300
+    parseInt(element.dataset.murphyAnimationDuration) || 300;
 
   var observerOptions = {
     threshold: 1,
-    rootMargin: `0px 0px ${appearanceDistance * -1}px 0px`,
-  }
+    rootMargin: `0px 0px ${appearanceDistance * -1}px 0px`
+  };
 
   var elementOptions = {
     element,
@@ -48,54 +51,54 @@ function startAnimation(element) {
     elementDistance,
     elementThreshold,
     ease,
-    delay,
-  }
+    delay
+  };
 
-  generateIntersectionObserver({ elementOptions, observerOptions })
+  generateIntersectionObserver({ elementOptions, observerOptions });
 }
 
 function generateIntersectionObserver({ elementOptions, observerOptions }) {
-  var element = elementOptions.element
-  var animationType = elementOptions.animationType
-  var threshold = observerOptions.elementThreshold
+  var element = elementOptions.element;
+  var animationType = elementOptions.animationType;
+  var threshold = observerOptions.elementThreshold;
   var observer = new IntersectionObserver(
     entries => {
       entries.forEach(entry => {
-        var { intersectionRatio } = entry
+        var { intersectionRatio } = entry;
         if (intersectionRatio > 0) {
-          generateAnimate(elementOptions, animationType)
-          observer.unobserve(entry.target)
+          generateAnimate(elementOptions, animationType);
+          observer.unobserve(entry.target);
         }
-      })
+      });
     },
     {
       ...observerOptions,
-      threshold,
+      threshold
     }
-  )
-  observer.observe(element)
+  );
+  observer.observe(element);
 }
 
 function generateAnimate(elementOptions, animationType) {
-  var element = elementOptions.element
-  var elementDistance = elementOptions.elementDistance
-  var animationDuration = elementOptions.animationDuration
-  var ease = elementOptions.ease
-  var delay = elementOptions.delay
+  var element = elementOptions.element;
+  var elementDistance = elementOptions.elementDistance;
+  var animationDuration = elementOptions.animationDuration;
+  var ease = elementOptions.ease;
+  var delay = elementOptions.delay;
 
   var options = {
-    elementDistance,
-  }
+    elementDistance
+  };
 
   if (element.animate) {
     element.animate(getAnimationType(animationType, options), {
       duration: animationDuration,
-      fill: 'forwards',
+      fill: "forwards",
       easing: ease,
-      delay,
-    })
+      delay
+    });
   } else {
-    cancelMurphy()
+    cancelMurphy();
   }
 }
 
@@ -103,61 +106,60 @@ function getAnimationType(animationType, options) {
   switch (animationType) {
     case BOTTOM_TO_TOP:
       return [
-        { opacity: '0', transform: `translateY(${options.elementDistance}px)` },
-        { opacity: '1', transform: 'translateY(0px)' },
-      ]
+        { opacity: "0", transform: `translateY(${options.elementDistance}px)` },
+        { opacity: "1", transform: "translateY(0px)" }
+      ];
     case TOP_TO_BOTTOM:
       return [
         {
-          opacity: '0',
-          transform: `translateY(-${options.elementDistance}px)`,
+          opacity: "0",
+          transform: `translateY(-${options.elementDistance}px)`
         },
-        { opacity: '1', transform: 'translateY(0px)' },
-      ]
+        { opacity: "1", transform: "translateY(0px)" }
+      ];
     case LEFT_TO_RIGHT:
       return [
         {
-          opacity: '0',
-          transform: `translateX(-${options.elementDistance}px)`,
+          opacity: "0",
+          transform: `translateX(-${options.elementDistance}px)`
         },
-        { opacity: '1', transform: 'translateX(0px)' },
-      ]
+        { opacity: "1", transform: "translateX(0px)" }
+      ];
     case RIGHT_TO_LEFT:
       return [
-        { opacity: '0', transform: `translateX(${options.elementDistance}px)` },
-        { opacity: '1', transform: 'translateX(0px)' },
-      ]
+        { opacity: "0", transform: `translateX(${options.elementDistance}px)` },
+        { opacity: "1", transform: "translateX(0px)" }
+      ];
     default:
       return [
-        { opacity: '0', transform: `translateY(60px)` },
-        { opacity: '1', transform: 'translateY(0px)' },
-      ]
+        { opacity: "0", transform: `translateY(60px)` },
+        { opacity: "1", transform: "translateY(0px)" }
+      ];
   }
 }
 
 function resetElements(elements) {
   elements.forEach(element => {
-    // var stay = element.dataset.murphyStay || false
-    // if (stay) return
-    element.style.opacity = 0
-    element.animate([{ opacity: '0' }, { opacity: '0' }], {
+    element.style.opacity = 0;
+    element.animate([{ opacity: "0" }, { opacity: "0" }], {
       duration: 1,
-      fill: 'forwards',
-    })
-  })
+      fill: "forwards"
+    });
+  });
 }
 
 function observerIsSupported() {
-  console.log(window)
   if (
-    !'IntersectionObserver' in window &&
-    !'IntersectionObserverEntry' in window &&
-    !'intersectionRatio' in window.IntersectionObserverEntry.prototype &&
+    window.IntersectionObserver &&
+    "IntersectionObserver" in window &&
+    "IntersectionObserverEntry" in window &&
+    "intersectionRatio" in window.IntersectionObserverEntry.prototype &&
     window.Animation
   ) {
-    return false
+    return true;
+  } else {
+    return false;
   }
-  return true
 }
 
-export { playMurphy, resetMurphy }
+export { playMurphy, resetMurphy };
